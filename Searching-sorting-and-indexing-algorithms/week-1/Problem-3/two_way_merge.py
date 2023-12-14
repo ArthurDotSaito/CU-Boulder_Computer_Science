b539a22d609b556c71109ad173750d40882c8d87
Arthur Saito <arthur_saito1998@hotmail.com>
2023-12-14 00:04:41 -0300

    fix identation

--- Searching-sorting-and-indexing-algorithms/week-1/Problem-3/two_way_merge.py
+++ Searching-sorting-and-indexing-algorithms/week-1/Problem-3/two_way_merge.py
@@ -1,8 +1,6 @@
 def twoWayMerge(lst1, lst2):
-    # Implement the two way merge algorithm on 
-    #          two ascending order sorted lists
-    # return a fresh ascending order sorted list that 
-    #          merges lst1 and lst2
+    # Implement the two way merge algorithm on two ascending order sorted lists
+    # return a fresh ascending order sorted list that merges lst1 and lst2
     i,j = 0,0
     result_list = []
     
@@ -19,8 +17,7 @@ def twoWayMerge(lst1, lst2):
     return result_list
 
 # given a list_of_lists as input, 
-#   if list_of_lists has 2 or more lists, 
-#        compute 2 way merge on elements i, i+1 for i = 0, 2, ...
+#   if list_of_lists has 2 or more lists,  compute 2 way merge on elements i, i+1 for i = 0, 2, ...
 #   return new list of lists after the merge
 #   Handle the case when the list size is odd carefully.
 def oneStepKWayMerge(list_of_lists):
@@ -34,3 +31,11 @@ def oneStepKWayMerge(list_of_lists):
         else: 
             ret_list_of_lists.append(list_of_lists[k-1])
     return ret_list_of_lists
+
+def kWayMerge(list_of_lists):
+    k = len(list_of_lists)
+    if k == 1:
+        return list_of_lists[0]
+    else:
+        new_list_of_lists = oneStepKWayMerge(list_of_lists)
+        return kWayMerge(new_list_of_lists)
